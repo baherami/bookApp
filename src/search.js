67596f6a7cfd827ba0cace668b7b07e10769fb8e
Ali Bahrami <baherami@gmail.com>
2017-08-15 03:47:10 -0700

    style: Name meaningfully the functions

--- src/search.js
+++ src/search.js
@@ -5,27 +5,29 @@ import Book from './book'
 //import serializeForm from 'form-serialize'
 class Search extends Component{
   state = {
-    query:''
+    books:''
   }
-  search=(e)=>{
+  searchBooks=(e)=>{
     e.preventDefault();
     console.log(e.target.value)
     //const searchTerm=serializeForm(e.target,{hash:true});
     let searchTerm=e.target.value
     BooksAPI.search(searchTerm,10).then((data)=>{
       console.log(data)
-      this.setState({query:data})
+      this.setState({books:data})
     })
     console.log(searchTerm);
     console.log('in search');
   }
+
   render(){
     const onBookChange=this.props.onBookChange;
-    let data=this.state.query
+    let searchedBooksList=this.state.books
+
     let searchResult=""
-    if ((data) &&!(data.error)){ // checking if result from server does not contain error
-      console.log(JSON.stringify(data))
-      searchResult=data.map(b=><Book key={b.id} info={b} onBookChange={onBookChange}/>)
+    if ((searchedBooksList) &&!(searchedBooksList.error)){ // checking if result from server does not contain error
+      console.log(JSON.stringify(searchedBooksList))
+      searchResult=searchedBooksList.map(b=><Book key={b.id} info={b} onBookChange={onBookChange}/>)
     }
 
     return(
@@ -33,7 +35,7 @@ class Search extends Component{
         <div className="search-books-bar">
           <Link className="close-search" to="/">Close</Link>
           <div className="search-books-input-wrapper">
-            <input type="text" onInput={this.search} name="search" placeholder="Search by title or author"/>
+            <input type="text" onInput={this.searchBooks} name="search" placeholder="Search by title or author"/>
           </div>
         </div>
         <div className="search-books-results">
